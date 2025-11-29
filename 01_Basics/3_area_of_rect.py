795f6e8d084545e43c7b07e631446cf972a4a373
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 18:38:32 +0530

    done

--- 01_Basics/3_area_of_rect.py
+++ 01_Basics/3_area_of_rect.py
@@ -1,5 +1,14 @@
 # Take length and width as input
-
+length = float(input(" Enter the length: "))
+width = float(input("Enter the width: "))
 # Calculate area
-
+area = length * width
 # Print the result
+print(" Float Area of rectangle:", area)
+  
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+# Calculate area
+area = length * width
+# Print the result
+print(" int Area of rectangle:", area)

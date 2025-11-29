0edf5b9bf257dede7198a0330f27db5dfc66a968
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 19:35:53 +0530

    done

--- 02_Data_and_operator/1_basic_calculator.py
+++ 02_Data_and_operator/1_basic_calculator.py
@@ -1,8 +1,17 @@
 # Take two numbers as input
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
 # Print sum
-
+print("Sum:", a + b)
 # Print difference
+print("Difference:", a - b)
+
 # Print product
+print("Product:", a * b)
+
 # Print quotient
+print("Quotient:", a / b)
 # Print remainder
+print("Remainder:", a % b)
+

2def45b5481712dff8822826efb1a7278197f04e
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 21:06:50 +0530

    done

--- 03_Indexing_and_slicing/2_slicing.py
+++ 03_Indexing_and_slicing/2_slicing.py
@@ -1,4 +1,10 @@
 # Input a string
+text = input("Enter a string: ")
 # Print the first 3 characters
+print("First 3 characters:", text[:3])
 # Print the last 3 characters
+print("Last 3 characters:", text[-3:])
 # Print characters from index 2 to 6
+print("Characters from index 2 to 6:", text[2:7])
+
+
--- 03_Indexing_and_slicing/3_step_reverse.py
+++ 03_Indexing_and_slicing/3_step_reverse.py
@@ -1,4 +1,8 @@
 # Input a string
+text = input("Enter a string: ")
 # Print the reversed string using slicing
+print("Reversed string:", text[::-1])
 # Print every second character using slicing (step = 2)
+print("Every second character:", text[::2])
 # Print characters at even indexes using slicing
+print("Characters at even indexes:", text[0::2])

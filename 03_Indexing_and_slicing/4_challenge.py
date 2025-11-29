f10ccc4440e1dfd82783aedd493d29bc54daf56d
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 21:31:25 +0530

    done

--- 03_Indexing_and_slicing/4_challenge.py
+++ 03_Indexing_and_slicing/4_challenge.py
@@ -1,4 +1,9 @@
 # Input an email address (like "student123@gmail.com")
+email = input("Enter your email address: ")
 # Slice out the username (everything before '@')
+username = email[:email.index('@')]
 # Remove the first and last character from that username
+trimmed_username = username[1:-1]
 # Print both: original username and trimmed username
+print("Original username:", username)
+print("Trimmed username:", trimmed_username)

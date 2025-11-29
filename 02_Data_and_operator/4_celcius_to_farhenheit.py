959c163ea383236009aa11a414d5ea47494b91c4
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 19:55:26 +0530

    done

--- 02_Data_and_operator/4_celcius_to_farhenheit.py
+++ 02_Data_and_operator/4_celcius_to_farhenheit.py
@@ -1,3 +1,6 @@
 # Input temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
 # Convert to Fahrenheit using formula
+fahrenheit = (celsius * 9/5) + 32
 # Print the result
+print("Temperature in Fahrenheit is:", fahrenheit)

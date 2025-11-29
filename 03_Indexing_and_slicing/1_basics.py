0d1bcade8735ba73c6196a1afe8b8c3d17120711
pathanfalak43@gmail.com <pathanfalak43@gmail.com>
2025-11-29 20:51:44 +0530

    done

--- 03_Indexing_and_slicing/1_basics.py
+++ 03_Indexing_and_slicing/1_basics.py
@@ -1,4 +1,9 @@
 # Input a string
+text = input("Enter a string: ")
 # Print first character
+print("First character:", text[0])
 # Print last character
+print("Last character:", text[-1])
 # Print middle character (use len() to find middle index)
+middle_index = len(text) // 2
+print("Middle character:", text[middle_index])
